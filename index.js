0118a31e585b018e9d70e3a6f6c208a99e9a3f0f
Jakub Paprocki <jakub@codeland.pl>
2020-11-15 21:23:40 +0100

    1.0.0

---
+++ index.js
@@ -0,0 +1,158 @@
+const _ = require('lodash');
+const Queue = require('./lib/queue');
+const RedisSMQ = require('rsmq');
+
+module.exports = function (sails) {
+
+    let self, config;
+    let queues = {};
+
+    return {
+        defaults: {
+            __configKey__: {
+                verbose: false,
+                defaultServer: {
+                    host: '127.0.0.1',
+                    port: 6379,
+                    options: {},
+                    ns: 'rsmq',
+                },
+                defaultOptions: {
+                    vt: 30,
+                    delay: 0,
+                    maxsize: 65536,
+                },
+            }
+        },
+        initialize: function () {
+            self = this;
+            config = sails.config[this.configKey];
+            sails.log.info('Initializing hook (`sails-hook-custom-queues`)');
+            _initQueues();
+        },
+        push: _push,
+        pop: _pop,
+        isEmpty: _isEmpty,
+    }
+
+    function _initQueues() {
+        let queuesCfg = _.omit(config, ['verbose', 'defaultServer','defaultOptions']);
+        for (let qname of _.keys(queuesCfg)) {
+            let queueCfg = queuesCfg[qname];
+            let serverCfg = _.defaults(queueCfg.server, config.defaultServer)
+            let queueSameServer = _.values(queues).find(q => {
+                return q.client.redis.options.host === serverCfg.host
+                    && Number(q.client.redis.options.port) === Number(serverCfg.port);
+            });
+            if (queueSameServer) {
+                serverCfg.client = queueSameServer.client.redis;
+            }
+            let queue = new Queue(serverCfg);
+            queue.connect()
+                .then(() => {
+                    queues[qname] = queue;
+                    queues[qname].qname = qname;
+                    let options = _.defaults(queueCfg.options, config.defaultOptions);
+                    return _createQueue(qname, options);
+                })
+                .catch(sails.log.error);
+        }
+    }
+
+    function _createQueue(qname, options) {
+        options = options || {};
+        let queue = queues[qname];
+        if (!queue) {
+            throw new Error(`Not registered queue '${qname}'`);
+        }
+        return queue.create({ ...options, qname,})
+            .then((result) => {
+                if (result) {
+                    if (config.verbose) {
+                        sails.log.info(`Queue '${qname}' has been created`);
+                    }
+                } else {
+                    throw new Error(`Queue '${qname}' not created`);
+                }
+            })
+            .catch(error => {
+                if (error.name === 'queueExists') {
+                    if (config.verbose) {
+                        sails.log.info(`Queue '${qname}' already exists`);
+                    }
+                    return;
+                }
+                sails.log.error(error);
+            });
+    }
+
+    function _push(qname, message, options) {
+        options = options || {};
+        let queue = queues[qname];
+        if (!queue) {
+            throw new Error(`Not registered queue '${qname}'`);
+        }
+        return queue.push(message, options)
+            .then((id) => {
+                if (id && config.verbose) {
+                    sails.log.info(`Message #${id} pushed to queue '${qname}'`);
+                }
+                return id;
+            })
+            .catch(error => {
+                if (error.name === 'queueNotFound') {
+                    if (config.verbose) {
+                        sails.log.info(`Queue '${qname}' not found, trying create...`);
+                    }
+                    return _createQueue(qname, config.queues[qname].options)
+                        .then(() => _push(qname, message, options));
+                }
+                sails.log.error(error);
+            });
+    }
+
+    function _pop(qname, options) {
+        options = options || {};
+        let queue = queues[qname];
+        if (!queue) {
+            throw new Error(`Not registered queue '${qname}'`);
+        }
+        return queue.pop(options)
+            .then((result) => {
+                if (result && config.verbose) {
+                    sails.log.info(`Message #${result.id} popped from queue '${qname}'`);
+                }
+                return result;
+            })
+            .catch(error => {
+                if (error.name === 'queueNotFound') {
+                    if (config.verbose) {
+                        sails.log.info(`Queue '${qname}' not found, trying create...`);
+                    }
+                    return _createQueue(qname, config.queues[qname].options)
+                        .then(() => _pop(qname, options));
+                }
+                sails.log.error(error);
+            });
+    }
+
+    function _isEmpty(qname) {
+        let queue = queues[qname];
+        if (!queue) {
+            throw new Error(`Not registered queue '${qname}'`);
+        }
+        return queue.getAttributes()
+            .then((attributes) => !attributes.msgs)
+            .catch(error => {
+                if (error.name === 'queueNotFound') {
+                    if (config.verbose) {
+                        sails.log.info(`Queue '${qname}' not found, trying create...`);
+                    }
+                    return _createQueue(qname, config.queues[qname].options)
+                        .then(() => _isEmpty(qname));
+                }
+                sails.log.error(error);
+            });
+    }
+
+}
